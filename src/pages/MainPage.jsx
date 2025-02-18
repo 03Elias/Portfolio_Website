ee7294d6b920bf321852a76d145f53deb5dea9db
03Elias <brofist.man43@gmail.com>
2025-02-18 16:29:34 +0100

    Temp fix for mainpage intro animation

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,16 +1,21 @@
 import "./MainPage.css";
+import EG_Logo from "../assets/EG.png"
 
 function MainPage() {
   return (
   <>
     <div className="main-page-container">
         <div className="anim-EG-container">
-            <h1 className="anim-EG">
-                <span className="anim-E">E</span>
-                <span className="anim-Lias">lias</span>
-                <span className="anim-G">G</span>
-                <span className="anim-Aghlasian">aghlasian</span>
-            </h1>
+        
+        <div className="anim-EG">
+                <h1 className="anim-E">E</h1>
+                <h1 className="anim-G">G</h1>
+            </div>
+            <h1 className="anim-Lias">lias</h1>
+            
+            <h1 className="anim-Aghlasian">aghlasian</h1>
+
+           
         </div>
     </div>
   </>
