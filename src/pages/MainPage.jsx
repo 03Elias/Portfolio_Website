50f3522a7a44a85034b29443d4a760df1e4336e4
03Elias <brofist.man43@gmail.com>
2025-02-18 17:53:45 +0100

    Fixed animation for mainpage intro

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,24 +1,19 @@
 import "./MainPage.css";
-import EG_Logo from "../assets/EG.png"
 
 function MainPage() {
   return (
-  <>
-    <div className="main-page-container">
+    <>
+      <div className="main-page-container">
         <div className="anim-EG-container">
-        
-        <div className="anim-EG">
-                <h1 className="anim-E">E</h1>
-                <h1 className="anim-G">G</h1>
-            </div>
-            <h1 className="anim-Lias">lias</h1>
-            
-            <h1 className="anim-Aghlasian">aghlasian</h1>
-
-           
+          <h1 className="anim-E">
+            E<span className="anim-Lias">lias</span>
+          </h1>
+          <h1 className="anim-G">
+            G<span className="anim-Aghlasian">aghlasian</span>
+          </h1>
         </div>
-    </div>
-  </>
+      </div>
+    </>
   );
 }
 
