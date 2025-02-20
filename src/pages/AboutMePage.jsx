a578bb8bbaa48a91991dce2c27dc10a19fcf6e72
03Elias <brofist.man43@gmail.com>
2025-02-20 23:27:16 +0100

    Fixed whole aboutmepage so everything works correctly apperance and logic wise!

--- src/pages/AboutMePage.jsx
+++ src/pages/AboutMePage.jsx
@@ -1,24 +1,51 @@
 import './AboutMePage.css'
+import KTH_Logo from "../assets/kth-connect.svg"
+import UNIU from "../assets/UGGLU.svg"
+import Algorithm from "../assets/algoData.svg"
 
 function AboutMePage () {
 
     return (
         <>
             <div className="aboutMe-page-container">
+
                 <div className="cardHolder">
-
-                    <div className='card'>
-                    <p>Test</p>
+                    <a href="https://github.com/danljungstrom/KTH-Connect" target="_blank" rel="noopener noreferrer">
+                        <div className='card first'>
+                            <img
+                                className="kth-connect-logo"
+                                src={KTH_Logo}
+                                alt="KTH-connect logo"
+                            />
+                            <p className="firstParagraph">KTH-Connect Application (Social Application For Students To Communicate At KTH)</p>
+                        </div>
+                    </a>
+                    <a href="https://github.com/Karl-Johan-Jacobson/Ladusvala" target="_blank" rel="noopener noreferrer">
+                    <div className='card second'>
+                        <img
+                            className="uniu-logo"
+                            src={UNIU}
+                            alt="UNIU owl logo"
+                        />
+                        <p className="secondParagraph">Student counselor to help find a relevant program based on personal interests <br/>(Application is available at uniu.nu.)</p>
                     </div>
-
-                    <div className='card'>
-                    <p>Test</p>
-                    </div>
-
-                    <div className='card'>
-                    <p>Test</p>
-                    </div>
+                    </a>
+                    <a href="https://github.com/03Elias/ID1021-Algoritmer-och-Datastrukturer/tree/main" target="_blank" rel="noopener noreferrer">
+                        <div className='card third'>
+                            <img
+                                className="algo-logo"
+                                src={Algorithm}
+                                alt="Binary Tree Algorithms logo"
+                            />
+                            <p className="thirdParagraph">A Java course where you learn to program algorithms and data structures </p>
+                        </div>
+                    </a>
                 </div>
+                    <div className="githubButtonContainer">
+                        <button className="githubButton" onClick={() => window.open("https://github.com/03Elias", "_blank")}>
+                            GitHub
+                        </button>
+                    </div>
             </div>
         </>
     );
