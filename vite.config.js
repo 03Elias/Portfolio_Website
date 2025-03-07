628a524c35281a00d7c8bd52a32c06181c417a8b
03Elias <brofist.man43@gmail.com>
2025-03-07 16:13:53 +0100

    Added GitHub Pages deployment configuration

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/Portfolio_Website/", // Replace with your actual GitHub repo name
 })
